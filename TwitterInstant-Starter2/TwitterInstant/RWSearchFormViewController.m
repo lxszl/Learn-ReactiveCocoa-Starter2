f289b48c1636b22fbe2d3e9a41aedd8b0fd9b9a9
liangxs <lxszl@126.com>
2016-09-23 16:16:54 +0800

    请求完成

--- TwitterInstant-Starter2/TwitterInstant/RWSearchFormViewController.m
+++ TwitterInstant-Starter2/TwitterInstant/RWSearchFormViewController.m
@@ -8,6 +8,17 @@
 
 #import "RWSearchFormViewController.h"
 #import "RWSearchResultsViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
+
+typedef NS_ENUM(NSInteger, RWTwitterInstantError) {
+    RWTwitterInstantErrorAccessDenied, 
+    RWTwitterInstantErrorNoTwitterAccounts, 
+    RWTwitterInstantErrorInvalidResponse 
+}; 
+ 
+static NSString * const RWTwitterInstantDomain = @"TwitterInstant";
 
 @interface RWSearchFormViewController ()
 
@@ -15,20 +26,67 @@
 
 @property (strong, nonatomic) RWSearchResultsViewController *resultsViewController;
 
+@property (strong, nonatomic) ACAccountStore *accountStore; 
+@property (strong, nonatomic) ACAccountType *twitterAccountType;
 @end
 
 @implementation RWSearchFormViewController
 
 - (void)viewDidLoad
 {
-  [super viewDidLoad];
-  
-  self.title = @"Twitter Instant";
-  
-  [self styleTextField:self.searchText];
-  
-  self.resultsViewController = self.splitViewController.viewControllers[1];
-  
+    [super viewDidLoad];
+    
+    self.title = @"Twitter Instant";
+    
+    [self styleTextField:self.searchText];
+    
+    self.resultsViewController = self.splitViewController.viewControllers[1];
+    //    @weakify(self)
+    //    [[self.searchText.rac_textSignal
+    //     map:^id(NSString *text) {
+    //
+    //         @strongify(self)
+    //         return [self isValidSearchText:text] ? [UIColor whiteColor] : [UIColor yellowColor];
+    //    }]
+    //    subscribeNext:^(UIColor *color) {
+    //        @strongify(self)
+    //        self.searchText.backgroundColor = color;
+    //    }];
+    
+    
+    @weakify(self)
+    RAC(self.searchText,backgroundColor) = [self.searchText.rac_textSignal
+                                            map:^id(NSString *text) {
+                                                @strongify(self)
+                                                return [self isValidSearchText:text] ? [UIColor whiteColor] : [UIColor yellowColor];
+                                            }];
+    
+    
+    self.accountStore = [[ACAccountStore alloc] init];
+    self.twitterAccountType = [self.accountStore
+                               accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    
+    [[[[[[self requestAccessToTwitterSignal]
+      then:^RACSignal *{
+        
+        return self.searchText.rac_textSignal;
+    }]
+      filter:^BOOL(NSString *text) {
+        
+          return [self isValidSearchText:text];
+    }]
+      flattenMap:^RACStream *(NSString *text) {
+        
+        return [self signalForSearchWithText:text];
+    }]
+      deliverOn:[RACScheduler mainThreadScheduler]]
+     subscribeNext:^(id value) {
+        
+        NSLog(@"%@",value);
+    } error:^(NSError *error) {
+        
+        NSLog(@"An error occurred: %@", error);
+    }];
 }
 
 - (void)styleTextField:(UITextField *)textField {
@@ -38,4 +96,77 @@
   textFieldLayer.cornerRadius = 0.0f;
 }
 
+
+- (BOOL)isValidSearchText:(NSString *)text {
+    return text.length > 2;
+}
+
+- (RACSignal *)requestAccessToTwitterSignal{
+    
+    NSError *accessError = [NSError errorWithDomain:RWTwitterInstantDomain code:RWTwitterInstantErrorAccessDenied userInfo:nil];
+    
+    @weakify(self)
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        
+        @strongify(self)
+        [self.accountStore requestAccessToAccountsWithType:self.twitterAccountType options:nil completion:^(BOOL granted, NSError *error) {
+            
+            if (!granted) {
+                
+                [subscriber sendError:accessError];
+            }else{
+                [subscriber sendNext:nil];
+                [subscriber sendCompleted];
+            }
+            
+        }];
+        
+        return nil;
+    }];
+}
+
+- (RACSignal *)signalForSearchWithText:(NSString *)text {
+    
+    NSError *noAccountsError = [NSError errorWithDomain:RWTwitterInstantDomain code:RWTwitterInstantErrorNoTwitterAccounts userInfo:nil];
+    NSError *invalidResponseError = [NSError errorWithDomain:RWTwitterInstantDomain code:RWTwitterInstantErrorInvalidResponse userInfo:nil];
+    @weakify(self);
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        
+        @strongify(self);
+        SLRequest *request = [self requestforTwitterSearchWithText:text];
+        NSArray *twitterAccounts = [self.accountStore accountsWithAccountType:self.twitterAccountType];
+        if (twitterAccounts.count==0) {
+            
+            [subscriber sendError:noAccountsError];
+        }else{
+            
+            [request setAccount:[twitterAccounts lastObject]];
+            [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+                
+                if (urlResponse.statusCode==200) {
+                    
+                    NSDictionary *timelineData = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingAllowFragments error:nil];
+                    [subscriber sendNext:timelineData];
+                    [subscriber sendCompleted];
+                }else{
+                    
+                    [subscriber sendError:invalidResponseError];
+                }
+            }];
+        }
+        
+        return nil;
+    }];
+}
+
+
+- (SLRequest *)requestforTwitterSearchWithText:(NSString *)text {
+    NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/search/tweets.json"];
+    NSDictionary *params = @{@"q" : text};
+    SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeTwitter
+                                            requestMethod:SLRequestMethodGET
+                                                      URL:url
+                                               parameters:params];
+    return request; 
+}
 @end
